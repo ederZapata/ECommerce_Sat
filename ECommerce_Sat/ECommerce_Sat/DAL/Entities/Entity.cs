042d3dbf840fb9419333141a6ca465eb568e20ef
ELDER ALBERTO ZAPATA BOLIVAR <elderzapata265446@correo.itm.edu.co>
2023-03-23 23:49:56 -0500

    NewCRUDCountriesController

--- ECommerce_Sat/ECommerce_Sat/DAL/Entities/Entity.cs
+++ ECommerce_Sat/ECommerce_Sat/DAL/Entities/Entity.cs
@@ -8,7 +8,11 @@ namespace ECommerce_Sat.DAL.Entities
 
         public virtual Guid Id { get; set; }
 
+        [Display(Name = "Fecha de creación")]
+
         public virtual string? CreatedDate { get; set; }
+
+        [Display(Name = "Fecha de modificación")]
 
         public  virtual string? ModifieldDate { get; set;} //Virtual - Override
     }
